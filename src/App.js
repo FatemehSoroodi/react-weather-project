893342e80352bea6357775d98ba0d4e92d0b3f44
Fatemeh <f.soroodi.s@gmail.com>
2023-08-09 22:13:14 +0330

    Footer added to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/FatemehSoroodi/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open Source Code
+        </a>
+        , by
+        <em>Fatemeh Soroodi</em>
+      </footer>
     </div>
   );
 }
